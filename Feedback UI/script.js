e712a18f434828c2d6ceef55bddb43966451d904
Iqra327 <iqrarasheed219@gmail.com>
2024-08-12 22:56:29 +0500

    Feedback UI

--- Feedback UI/script.js
+++ Feedback UI/script.js
@@ -1,15 +1,31 @@
-const reviewBtn = document.querySelector('.js-review-btn');
-const rating = document.querySelector('.js-rating');
-const emojiContainer = document.querySelector('.js-emoji-container');
+const reviewBtnElement = document.querySelector('.js-review-btn');
+const ratingElements = document.querySelectorAll('.js-rating');
+const containerElement = document.querySelector('.js-container');
 
-reviewBtn.addEventListener('click', sendFeedback());
+let selectedRating = '';
 
-function sendFeedback(){
-  rating.addEventListener('click', () => {
-    rating.classList.add('active');
-    if(rating.childNodes[1].textContent = 'Unhappy'){
-      console.log('unhappy');
-    } 
+ratingElements.forEach((ratingEl) => {
+  ratingEl.addEventListener('click', () => {
+    removeActive();
+    ratingEl.classList.add('active');
+    selectedRating = ratingEl.innerText;
+  })
+})
+
+reviewBtnElement.addEventListener('click', () => {
+  if(selectedRating){
+    containerElement.innerHTML = `
+      <strong>Thank You!</strong>
+      <br><br>
+      <strong>Feedback: ${selectedRating}</strong>
+      <br><br>
+      <p>We'll use your feedback to improve our customer support.</p>
+    `;
+  }
+})
+
+function removeActive(){
+  ratingElements.forEach((ratingEl) => {
+    ratingEl.classList.remove('active');
   })
 }
-
