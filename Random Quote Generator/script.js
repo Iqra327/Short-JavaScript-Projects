64a304cbb74dcd6053c81147a673f6bf06e1abab
Iqra327 <iqrarasheed219@gmail.com>
2024-08-14 00:09:19 +0500

    in-process

---
+++ Random Quote Generator/script.js
@@ -0,0 +1,5 @@
+const quoteElement = document.querySelector('.js-quote');
+const quoteWriterElement = document.querySelector('.js-quote-writer');
+const quoteBtnElement = document.querySelector('.js-quote-btn');
+
+quoteBtnElement.addEventListener('click', )
