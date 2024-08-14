dc6a11f0d4ec7fa0e22637e38c8e4199ce629735
Iqra327 <iqrarasheed219@gmail.com>
2024-08-14 10:12:55 +0500

    Random quote generator using api

--- Random Quote Generator/script.js
+++ Random Quote Generator/script.js
@@ -1,5 +1,33 @@
-const quoteElement = document.querySelector('.js-quote');
+const quoteContainer = document.querySelector('.js-container');
 const quoteWriterElement = document.querySelector('.js-quote-writer');
 const quoteBtnElement = document.querySelector('.js-quote-btn');
+const quoteElement = document.querySelector('.js-quote');
 
-quoteBtnElement.addEventListener('click', )
+const options = {
+  headers: {
+    'X-Api-Key' : 'TJRPFFQrTcnQJmzQ+k3GJg==4nUeSK4gvRfCl18N' 
+  }
+};
+
+const api_url = 'https://api.api-ninjas.com/v1/quotes?category=inspirational';
+
+async function fetchApi(){
+  try {
+    quoteElement.innerText = 'Updating...';
+    quoteWriterElement.innerText = 'Updating...';
+    quoteBtnElement.innerText = 'Loading...';
+
+    const response =await fetch(api_url, options);
+    const data = await response.json();
+
+    quoteElement.innerText = data[0].quote;
+    quoteWriterElement.innerText = `~ ${data[0].author}`;
+    quoteBtnElement.innerText = 'Get Quote';
+    } 
+    catch (error) {
+      quoteContainer.innerHTML = 
+      `<h3>OOPS!! <br> Something went wrong, Try again later.</h3>`;
+    }
+};
+
+quoteBtnElement.addEventListener('click',fetchApi);
